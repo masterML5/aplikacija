b4998b8e1bcaba83ef2d720e98038acb2bd9ef01
masterML5 <milosjelic98@gmail.com>
2022-04-23 19:58:12 +0200

    ok

--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import {InjectRepository} from '@nestjs/typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Administrator } from 'entities/administrator.entity';
-import {Repository} from 'typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AdministratorService {
